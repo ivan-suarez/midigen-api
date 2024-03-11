0c7e6ea46aaa2f44aac44c01a3d6ffe6dc057865
ivan-suarez <ivansuarezcast@gmail.com>
2024-03-11 19:14:45 +0100

    Modify model and improve post-processing

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def generate():
-    model = load_model('bach_one_note.keras')
+    model = load_model('bach_one_note-0_2.keras')
 
     starting_sequence = [(2.75, 3.25, 72, 50),
     (3.25, 4.0, 72, 50),
@@ -59,12 +59,14 @@ def generate():
     (25.75, 25.75, 81, 56),
     (25.75, 26.0, 83, 49)]
 
-    starting_sequence_one = [(2.75, 3.25, 72, 50)]
+    starting_sequence_one = [(2.75, 3.25, 64, 21)]
 
     seed_index = 0
     seed_sequence = starting_sequence_one # Starting with an existing sequence as seed
     num_notes_to_generate = 100  # Number of notes you want to generate
     generated_notes = generate_notes(model, seed_sequence, num_notes_to_generate)
+    print(generated_notes)
+    create_midi_from_notes(generated_notes)
     return generated_notes
 
 def generate_notes(model, seed_sequence, num_notes_to_generate):
@@ -98,23 +100,36 @@ def create_midi_from_notes(notes, output_file='generated_sequence.mid'):
     last_event_time = 0
     
     for note in notes:
-        start_time, end_time, pitch, velocity = map(int, note)
-        
+        print(note)
+        start_time, duration, pitch, velocity = map(float, note)
+        print(duration)
+        duration = abs(duration*10)
+        end_time = start_time +  duration
+        time_step = 0.25
+        start_time = round(start_time / time_step) * time_step
+        end_time = round(end_time / time_step) * time_step
         # Ensure non-negative delta times
-        delta_time_on = max(0, start_time - last_event_time)
-        delta_time_off = max(0, end_time - start_time)
+        #delta_time_on = max(0, start_time - last_event_time)
+        #delta_time_off = max(0, end_time - start_time)
+        # Assuming a standard conversion, adjust based on your actual data and tempo
+        milliseconds_per_tick = 5 / midi.ticks_per_beat  # For 120 BPM and 480 TPB
+        delta_time_on = int(max(0, start_time - last_event_time)/milliseconds_per_tick)
+        delta_time_off = int(max(0, end_time - start_time)/milliseconds_per_tick)
+
         
         # Update last event time for the next iteration
         # For note_on, this is the start time of the current note
         last_event_time = start_time
-        
+        print(start_time)
+        print(duration)
+        print(end_time)
         # Add the note_on message
-        track.append(Message('note_on', note=pitch, velocity=velocity, time=delta_time_on))
+        track.append(Message('note_on', note=int(pitch), velocity=int(velocity), time=delta_time_on))
         # For note_off, update the last event time to be the end time of the current note
         last_event_time = end_time
         
         # Add the note_off message with time since the note_on
-        track.append(Message('note_off', note=pitch, velocity=0, time=delta_time_off))
+        track.append(Message('note_off', note=int(pitch), velocity=0, time=delta_time_off))
     
     midi.save(output_file)
   #  return output_file
--- web.py
+++ web.py
@@ -8,8 +8,8 @@ CORS(app)
 @app.route('/')
 def home():
     generated_sequence = main.generate()
-    output = main.midi_to_json(generated_sequence)
-    return send_file('my_generated_midi.mid', mimetype='audio/midi')
+    #output = main.midi_to_json(generated_sequence)
+    return send_file('generated_sequence.mid', mimetype='audio/midi')
 
 if __name__ == '__main__':
     app.run(debug=True)
